84f8b0db998239a83995974a05ca1201ddb9e0ce
VincentA <vincent.amstoutz@outlook.fr>
2020-01-29 11:35:16 +0100

    Add env symfony 4.4

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
-Si ça s'affiche c'est que ça fonctionne ;)
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<iframe src="https://giphy.com/embed/j4q4h9uWKWwnYT1k3Z" width="480" height="216" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/j4q4h9uWKWwnYT1k3Z">Bébé Yoda qui découvre notre installation</a></p>
+<?php
+
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
