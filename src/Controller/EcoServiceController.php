f80a2d47545973db32f3619e4fbce4da546ea117
Martin Soenen <martin.soenen@outlook.com>
2020-02-03 18:50:13 +0100

    KAN-94 - Add some controllers and views

--- src/Controller/EcoServiceController.php
+++ src/Controller/EcoServiceController.php
@@ -16,4 +16,34 @@ class EcoServiceController extends AbstractController
             'controller_name' => 'EcoServiceController',
         ]);
     }
+
+    /**
+     * @Route("/faq", name="faq")
+     */
+    public function faq()
+    {
+        return $this->render('eco_service/faq.html.twig', [
+            'controller_name' => 'EcoServiceController',
+        ]);
+    }
+    
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('eco_service/contact.html.twig', [
+            'controller_name' => 'EcoServiceController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/accueil", name="admin")
+     */
+    public function admin_index()
+    {
+        return $this->render('eco_service/admin_index.html.twig', [
+            'controller_name' => 'EcoServiceController',
+        ]);
+    }
 }
