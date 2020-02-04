0817e90d0c46cb5e4d8f4beac439f2cb1ae546f8
sylbugey <sylvain.platet@gmail.com>
2020-02-04 09:18:33 +0100

    KAN-41- Access to information about "Eco-Service" (Who are we?)

--- src/Controller/EcoServiceController.php
+++ src/Controller/EcoServiceController.php
@@ -16,4 +16,23 @@ class EcoServiceController extends AbstractController
             'controller_name' => 'EcoServiceController',
         ]);
     }
+    /**
+     * @Route("/CGU-CGV", name="CGU-CGV")
+     */
+    public function CGU()
+    {
+        return $this->render('eco_service/CGU.html.twig', [
+            'controller_name' => 'EcoServiceController',
+        ]);
+    }
+
+    /**
+     * @Route("/qui-sommes-nous", name="qui-sommes-nous")
+     */
+    public function qui_sommes_nous()
+    {
+        return $this->render('eco_service/qui_sommes_nous.html.twig', [
+            'controller_name' => 'EcoServiceController',
+        ]);
+    }
 }
