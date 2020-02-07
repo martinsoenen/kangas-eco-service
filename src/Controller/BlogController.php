8d19f3f7da573d06f373a07e3e47a3891f2e1e7c
sylbugey <sylvain.platet@gmail.com>
2020-02-07 17:35:30 +0100

    KAN - 36  - Création article blog Diy

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\File;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaTypeType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -40,8 +48,12 @@ class BlogController extends AbstractController
      */
     public function showAdminBlog()
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
         return $this->render('blog/adminBlog.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles,
         ]);
     }
       /**
@@ -51,6 +63,41 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/adminArticles.html.twig', [
             'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article/add", name="add_article_admin"))
+     */
+    public function addAdminArticles(Article $article = null,Request $request,EntityManagerInterface $manager)
+    {
+        if(!$article) {
+            $article = new Article();
+        }
+        $form = $this->createFormBuilder($article)
+
+            ->add('Titre', TextType::class,array('required'  => true))
+            ->add('Text', TextareaType::class,array('required'  => true))
+            ->add('Image', FileType::class,array('required'  => true))
+            ->getForm();
+
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted()&& $form->isValid()) {
+            $article->setDate(new \DateTime());
+            $article->setUtilisateurAdmin(null);
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_admin');
+        }
+
+        return $this->render('blog/ajouterArticles.html.twig', [
+            'controller_name' => 'BlogController',
+            'formArticle'=> $form->createView(),
+            'editMode'=>$article->getId() !== null,
         ]);
     }
 }
