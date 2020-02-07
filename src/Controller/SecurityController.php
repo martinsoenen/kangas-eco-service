46ab60e18c92520c334201f77afd350474b28177
VincentA <vincent.amstoutz@outlook.fr>
2020-02-07 17:31:16 +0100

    KAN-30,34,65 - Finish signin & login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 use App\Entity\Utilisateur;
+use App\Entity\Adresse;
+use App\Form\AdresseType;
 use App\Form\RegistrationTypeClient;
 use App\Form\RegistrationTypeEntreprise;
 class SecurityController extends AbstractController
@@ -19,34 +21,38 @@ class SecurityController extends AbstractController
      */
     public function SignIn(Request $request, UserPasswordEncoderInterface $encoder)
     {   
-        $Utilisateur = new Utilisateur;
+        $Utilisateur = new Utilisateur();
+        $Adresse = new Adresse();
         
         $typeCompte = $this->createFormBuilder($Utilisateur)
                     ->add('utilisateurType', ChoiceType::class, array(
-                'label' => false,
-                'placeholder' => 'Type de compte',
-                'choices' => array(
-                    'Particulier' => 'client',
-                    'Professionnel' => 'pro',
-                ),
-                'required' => true
-            ))
+                    'label' => false,
+                    'placeholder' => 'Type de compte',
+                    'choices' => array(
+                        'Particulier' => 'client',
+                        'Professionnel' => 'pro',
+                    ),
+                    'required' => true
+                 ))
             ->getForm();
-        $formClient = $this->createForm(RegistrationTypeClient::class, $Utilisateur);
+        $formClient = $this->createForm(RegistrationTypeClient::class,$Utilisateur);
         $formEntreprise = $this->createForm(RegistrationTypeEntreprise::class, $Utilisateur);
+        $formAdresse = $this->createForm(AdresseType::class, $Adresse);
+
         $formClient->handleRequest($request);
         $formEntreprise->handleRequest($request);
+         $formAdresse->handleRequest($request);
 
         $em = $this->getDoctrine()->getManager();
         
         if ($formClient->isSubmitted() && $formClient->isValid()) { 
             $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
             $Utilisateur->setPassword($hash);
-            $Utilisateur->setUtilisateurType("client");
+            $Utilisateur->setUtilisateurType("client");          
             $em->persist($Utilisateur);
             $em->flush();
         }
-
+        
         if ($formEntreprise->isSubmitted() && $formEntreprise->isValid()) { 
             $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
             $Utilisateur->setPassword($hash);
@@ -54,11 +60,20 @@ class SecurityController extends AbstractController
             $em->persist($Utilisateur);
             $em->flush();
         }
+        if ($formAdresse->isSubmitted() && $formAdresse->isValid()) { 
+            dump($Utilisateur);
+            $Adresse->setUtilisateur($Utilisateur);
+            $em->persist($Adresse);
+            $em->flush();
+        }
+
+         $Adresse->setUtilisateur($Utilisateur);
 
         return $this->render('security/signin.html.twig', [
             'formClient' => $formClient->createView(),
-            'formEntreprise' =>$formEntreprise->createView(),
-            'typeCompte' => $typeCompte->createView(),
+            'formEntreprise' =>$formEntreprise->createView(),                       
+            'formAdresse' => $formAdresse->createView(),
+            'typeCompte' => $typeCompte->createView(), 
             'controller_name' => 'SecurityController',
         ]);
     }
