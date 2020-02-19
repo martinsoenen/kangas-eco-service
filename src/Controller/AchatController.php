a1c83bd65ac8c3553965413064db4fe3c545c31f
Martin Soenen <martin.soenen@outlook.com>
2020-02-19 01:25:49 +0100

    KAN-23 - First PayPal API call

--- src/Controller/AchatController.php
+++ src/Controller/AchatController.php
@@ -2,9 +2,22 @@
 
 namespace App\Controller;
 
+require '../vendor/autoload.php';
+
 use App\Service\Panier\PanierService;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Transaction;
+use PayPal\Rest\ApiContext;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class AchatController extends AbstractController
 {
@@ -23,8 +36,62 @@ class AchatController extends AbstractController
     /**
      * @Route("/panier/paiement", name="panier_paiement")
      */
-    public function paiement()
+    public function paiement(PanierService $panierService)
     {
+        $credentials = [
+            'id' => 'Ae0q9Y6VL5tsv0vcBvzBMv3kjg7mM50yooD8C9u2nm1HmVa5pcCa9GH-Ov7swbpl1CHru_D2G_GXCQ4O',
+            'secret' => 'EFN_usuuBumAEyMgasVcamuZCaimCZ7JJzCWqsFbYKZ08HhQ6y43jENMHLJrk8qHhYfQRzXnt2SBYVHI'
+        ];
+        $apiContext = new ApiContext(
+            new OAuthTokenCredential($credentials['id'],$credentials['secret']
+            )
+        );
+
+        // On construit notre appel à l'API PayPal
+
+        $list = new ItemList();
+        $totalPrice = 0;
+        foreach ($panierService->getPanierComplet() as $product) {
+            $item = (new Item())
+                ->setName($product['product']->getNomProduit())
+                ->setPrice(round($product['product']->getPrixUnitaireHT() * ( 1 + $product['product']->getTauxTVA()), 2))
+                ->setCurrency('EUR')
+                ->setQuantity($product['quantity']);
+            $list->addItem($item);
+
+            $totalPrice += ($product['quantity'] * round(($product['product']->getPrixUnitaireHT() * ( 1 + $product['product']->getTauxTVA())), 2));
+        }
+
+        $details = (new Details())
+            ->setSubtotal($totalPrice);
+//            TODO:Ajouter la TVA
+
+        $amount = (new Amount())
+            ->setTotal($totalPrice)
+            ->setCurrency('EUR')
+            ->setDetails($details);
+
+        $transaction = (new Transaction())
+            ->setItemList($list)
+            ->setDescription('Achat sur le site Eco-Service')
+            ->setAmount($amount);
+
+        $payment = new Payment();
+        $payment->setTransactions([$transaction]);
+        $payment->setIntent('sale');
+        $redirectUrls = (new RedirectUrls())
+            ->setReturnUrl($this->generateUrl('panier_paiement', [], UrlGenerator::ABSOLUTE_URL))
+            ->setCancelUrl($this->generateUrl('panier', [], UrlGenerator::ABSOLUTE_URL));
+        $payment->setRedirectUrls($redirectUrls);
+        $payment->setPayer((new Payer())->setPaymentMethod('paypal'));
+
+        try {
+            $payment->create($apiContext);
+            header('Location: ' . $payment->getApprovalLink());
+        } catch (\PayPal\Exception\PayPalConnectionException $e) {
+            dump(json_decode($e->getData()));
+        }
+
         return $this->render('achat/paiement.html.twig', [
             'controller_name' => 'AchatController',
         ]);
