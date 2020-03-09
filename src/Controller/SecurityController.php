f75d0c10ad4fead1771b1ff444331de13813829b
VincentA <vincent.amstoutz@outlook.fr>
2020-03-09 17:50:25 +0100

    KAN-24, KAN-37 - Forgot password

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 use App\Entity\Utilisateur;
 use App\Entity\Adresse;
@@ -85,12 +87,82 @@ class SecurityController extends AbstractController
     /**
      * @Route("/connexion/mot-de-passe-oublie", name="security_forgot")
      */
-    public function passwordForget()
+    public function passwordForget(Request $request, UserPasswordEncoderInterface $encoder,
+                                     \Swift_Mailer $mailer,
+                                    TokenGeneratorInterface $tokenGenerator)
     {
+         if ($request->isMethod('POST')) {
+
+            $email = $request->request->get('email');
+
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository(Utilisateur::class)->findOneByEmail($email);
+
+            if ($user === null) {
+                $this->addFlash('danger', 'Email Inconnu');
+                return $this->redirectToRoute('home');
+            }
+            $token = $tokenGenerator->generateToken();
+
+            try{
+                $user->setTokenPassword($token);
+                $em->flush();
+            } catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('home');
+            }
+
+            $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+          
+            $message = (new \Swift_Message('Mot de passe oublié'))
+                ->setTo($user->getEmail())
+                ->setFrom('contact@kangas.fr')
+                ->setBody(
+                    "Cliquez ici pour réinitialiser votre mot de passe : " . $url,
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('notice', 'Un mail vient de vous être envoyé pour la réinitialisation ');
+
+            return $this->redirectToRoute('home');
+        }
+        
         return $this->render('security/passwordforget.html.twig', [
             'controller_name' => 'SecurityController',
         ]);
     }
+
+    /**
+     * @Route("/reinitialiser-mot-de-passe/{token}", name="app_reset_password")
+     */
+    public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $em->getRepository(Utilisateur::class)->findOneByTokenPassword($token);
+
+            if ($user === null) {
+                $this->addFlash('danger', 'Token Inconnu');
+                return $this->redirectToRoute('home');
+            }
+
+            $user->setTokenPassword(null);
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $em->flush();
+
+            $this->addFlash('notice', 'Mot de passe mis à jour');
+
+            return $this->redirectToRoute('home');
+        }else {
+
+            return $this->render('security/resetPassword.html.twig', ['token' => $token]);
+        }
+    }
+
     /**
      * @Route("/deconnexion/", name="security_logout")
      */
