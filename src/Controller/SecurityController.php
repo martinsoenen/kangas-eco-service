c7666bb534194de2add6f2f023b0a88bdaf80f1a
VincentA <vincent.amstoutz@outlook.fr>
2020-03-09 14:17:45 +0100

    KAN-112 - Fix Menu and Redirection for Security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,61 +21,66 @@ class SecurityController extends AbstractController
      */
     public function SignIn(Request $request, UserPasswordEncoderInterface $encoder)
     {   
-        $Utilisateur = new Utilisateur();
-        $Adresse = new Adresse();
-        
-        $typeCompte = $this->createFormBuilder($Utilisateur)
-                    ->add('utilisateurType', ChoiceType::class, array(
-                    'label' => false,
-                    'placeholder' => 'Type de compte',
-                    'choices' => array(
-                        'Particulier' => 'client',
-                        'Professionnel' => 'pro',
-                    ),
-                    'required' => true
-                 ))
-            ->getForm();
-        $formClient = $this->createForm(RegistrationTypeClient::class,$Utilisateur);
-        $formEntreprise = $this->createForm(RegistrationTypeEntreprise::class, $Utilisateur);
-        $formAdresse = $this->createForm(AdresseType::class, $Adresse);
+        if($this->getUser() == null){
+            $Utilisateur = new Utilisateur();
+            $Adresse = new Adresse();
+            
+            $typeCompte = $this->createFormBuilder($Utilisateur)
+                        ->add('utilisateurType', ChoiceType::class, array(
+                        'label' => false,
+                        'placeholder' => 'Type de compte',
+                        'choices' => array(
+                            'Particulier' => 'client',
+                            'Professionnel' => 'pro',
+                        ),
+                        'required' => true
+                    ))
+                ->getForm();
+            $formClient = $this->createForm(RegistrationTypeClient::class,$Utilisateur);
+            $formEntreprise = $this->createForm(RegistrationTypeEntreprise::class, $Utilisateur);
+            $formAdresse = $this->createForm(AdresseType::class, $Adresse);
 
-        $formClient->handleRequest($request);
-        $formEntreprise->handleRequest($request);
-         $formAdresse->handleRequest($request);
+            $formClient->handleRequest($request);
+            $formEntreprise->handleRequest($request);
+            $formAdresse->handleRequest($request);
 
-        $em = $this->getDoctrine()->getManager();
-        
-        if ($formClient->isSubmitted() && $formClient->isValid()) { 
-            $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
-            $Utilisateur->setPassword($hash);
-            $Utilisateur->setUtilisateurType("client");          
-            $em->persist($Utilisateur);
-            $em->flush();
-        }
-        
-        if ($formEntreprise->isSubmitted() && $formEntreprise->isValid()) { 
-            $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
-            $Utilisateur->setPassword($hash);
-            $Utilisateur->setUtilisateurType("pro");
-            $em->persist($Utilisateur);
-            $em->flush();
-        }
-        if ($formAdresse->isSubmitted() && $formAdresse->isValid()) { 
-            dump($Utilisateur);
+            $em = $this->getDoctrine()->getManager();
+            
+            if ($formClient->isSubmitted() && $formClient->isValid()) { 
+                $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
+                $Utilisateur->setPassword($hash);
+                $Utilisateur->setUtilisateurType("client");          
+                $em->persist($Utilisateur);
+                $em->flush();
+            }
+            
+            if ($formEntreprise->isSubmitted() && $formEntreprise->isValid()) { 
+                $hash = $encoder->encodePassword($Utilisateur, $Utilisateur->getPassword());    
+                $Utilisateur->setPassword($hash);
+                $Utilisateur->setUtilisateurType("pro");
+                $em->persist($Utilisateur);
+                $em->flush();
+            }
+            if ($formAdresse->isSubmitted() && $formAdresse->isValid()) { 
+                $Adresse->setUtilisateur($Utilisateur);
+                $em->persist($Adresse);
+                $em->flush();
+            }
+
             $Adresse->setUtilisateur($Utilisateur);
-            $em->persist($Adresse);
-            $em->flush();
+
+            return $this->render('security/signin.html.twig', [
+                'formClient' => $formClient->createView(),
+                'formEntreprise' =>$formEntreprise->createView(),                       
+                'formAdresse' => $formAdresse->createView(),
+                'typeCompte' => $typeCompte->createView(), 
+                'controller_name' => 'SecurityController',
+            ]);
         }
-
-         $Adresse->setUtilisateur($Utilisateur);
-
-        return $this->render('security/signin.html.twig', [
-            'formClient' => $formClient->createView(),
-            'formEntreprise' =>$formEntreprise->createView(),                       
-            'formAdresse' => $formAdresse->createView(),
-            'typeCompte' => $typeCompte->createView(), 
-            'controller_name' => 'SecurityController',
-        ]);
+        else{
+            $this->addFlash('error', 'Vous êtes déja connecté !');
+            return $this->redirectToRoute('home');
+        }
     }
     /**
      * @Route("/connexion/mot-de-passe-oublie", name="security_forgot")
@@ -97,8 +102,15 @@ class SecurityController extends AbstractController
      * @Route("/connexion/", name="security_login")
      */
     public function login(){
-        return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+
+        if($this->getUser() == null){
+            return $this->render('security/login.html.twig', [
+                'controller_name' => 'SecurityController',
+            ]);
+        }
+        else{
+            $this->addFlash('error', 'Vous êtes déja connecté !');
+            return $this->redirectToRoute('home');
+        }
     }
 }
