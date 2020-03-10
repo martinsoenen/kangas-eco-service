2e4e6a58af5d1fd47d5fcbaeaef96e358b87a533
khadidja <khadidjal71@gmail.com>
2020-03-10 21:07:12 +0100

    Lot 2 - le formulaire de devis fonctionne !! :)

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use App\Entity\CategorieCollecte;
 use App\Entity\ObjetCollecte;
 use App\Form\CategorieCollecteType;
+use App\Form\ContactDevisType;
+use App\Form\ContactGeneralType;
 use App\Form\ObjetCollecteType;
+use App\Repository\ObjetCollecteRepository;
+use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,43 +32,36 @@ class EntrepriseController extends AbstractController
     /**
      * @Route("/entreprise/devis", name="entreprise_devis")
      */
-    public function devis(Request $request)
+    public function devis(Request $request, \Swift_Mailer $mailer)
     {
-        $form = $this->createForm(CategorieCollecteType::class)
-            ->add('nom', EntityType::class, [
-            'class' => CategorieCollecte::class,
-            'choice_label' => 'nom',
-            'label' => 'Catégorie collectés',
-    ]);
+        $form = $this->createForm(ContactDevisType::class);
 
-        if ($request->isXmlHttpRequest()) {
-
-            $categorie = $request->get('categorie');
-            $form = $this->createForm(CategorieCollecteType::class)
-                ->add('nom', EntityType::class, [
-                    'class' => ObjetCollecte::class,
-                    'choice_label' => 'nom',
-                    'label' => 'Objets collectés',
-                    'query_builder' => function (EntityRepository $er) {
-                        return $er->createQueryBuilder('o')
-                            ->where('o.CategorieCollecte = :val')
-                            ->SetParameter('val', 'Electromenager');
-              },
-              ]);
-        }
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $categ = $form->getData();
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($categ);
-            $entityManager->flush();
+            $data = $form->getData();
+
+            $message = (new \Swift_Message('Demande de devis par une entreprise'))
+                ->setTo('devis@kangas.fr')
+                ->setFrom($data['email'])
+                ->setBody('Message envoyé par ' . $data['nom'] . ', représentant l\'entreprise ' . $data['entreprise'] .
+                    '<br/>Adresse d\'enlèvement : ' . $data['rue'] . ' - ' . $data['cp'] . ' - ' . $data['ville'] .
+                    '<br>Date d\'enlèvement : ' . date_format($data['date'],"Y/m/d") .
+                    '<br/>Objets à collecter : ' . $data['produits'] .
+                    '<br/>Commentaire supplémentaire : ' . $data['commentaire']
+                    ,'text/html'
+               );
+
+            $mailer->send($message);
+
+            $this->addFlash('notice', 'Votre email a bien été envoyé. Nous vous repondrons au plus vite ');
+
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('entreprise/devis.html.twig', [
             'controller_name' => 'EntrepriseController',
-            //'objetCollectes' => $objetCollectes,
             'form' => $form->createView(),
-            //'formObjet' => $formObjet->createView(),
         ]);
     }
 
---
+++ src/Form/ContactDevisType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use \Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+
+class ContactDevisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('entreprise', TextType::class,array(
+                'label' => 'Entreprise',
+                'required' => true,
+            ))
+            ->add('nom', TextType::class,array(
+                'label' => 'Nom et prénom du représentant',
+                'required' => true,
+            ))
+            ->add('email', EmailType::class,array(
+                'label' => 'Email de contact',
+                'required' => true,
+            ))
+            ->add('produits', TextareaType::class,array(
+                'label' => 'Objets à collecter',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Indiquez ici les objets à venir chercher et leur quantité',
+                ]
+            ))
+            ->add('rue', TextType::class,array(
+                'label' => 'Rue',
+                'required' => true,
+            ))
+            ->add('ville', TextType::class,array(
+                'label' => 'Ville',
+                'required' => true,
+            ))
+            ->add('cp', IntegerType::class,array(
+                'label' => 'Code Postal',
+                'required' => true,
+            ))
+            ->add('date', DateType::class,array(
+                'label' => 'Date',
+                'required' => true,
+            ))
+            ->add('commentaire', TextareaType::class,array(
+                'label' => 'Commentaire',
+                'required' => false,
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Envoyer',
+                'attr' => array('title' => 'Envoyer l\'email' ,'class' => 'btn btn-outline-success'
+                ),
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
