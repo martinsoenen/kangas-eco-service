ca5207a7f05e203b9a0d279585f4e902517cae24
Martin Soenen <martin.soenen@outlook.fr>
2021-03-19 22:38:48 +0100

    Fix repositories because of a strange exception

--- src/Repository/CategorieBlogRepository.php
+++ src/Repository/CategorieBlogRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CategorieBlog;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method CategorieBlog|null find($id, $lockMode = null, $lockVersion = null)
